3138d951e91b7d65a1f67639e3b07eb9dc5c4f53
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2023-05-02 19:13:06 -0700

    Add project files.

---
+++ Brain/PatrolAssaultLayer.cs
@@ -0,0 +1,32 @@
+﻿using DrakiaXYZ.BigBrain.Brains;
+using EFT;
+
+namespace DrakiaXYZ.BrainTest.Brain
+{
+    internal class PatrolAssaultLayer : CustomLayer
+    {
+        public PatrolAssaultLayer(BotOwner botOwner, int priority) : base(botOwner, priority)
+        {
+        }
+
+        public override string GetName()
+        {
+            return "PatrolAssault";
+        }
+
+        public override Action GetNextAction()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool IsActive()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool IsCurrentActionEnding()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ BrainTestPlugin.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using DrakiaXYZ.BigBrain.Brains;
+using DrakiaXYZ.BrainTest.Brain;
+using System.Collections.Generic;
+
+namespace DrakiaXYZ.BrainTest
+{
+    [BepInPlugin("xyz.drakia.braintest", "DrakiaXYZ-BrainTest", "0.1.0")]
+    [BepInDependency("xyz.drakia.bigbrain", "^0.1.0")]
+    public class BrainTestPlugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            BrainManager.AddCustomLayer(typeof(PatrolAssaultLayer), new List<string>() { "Assault" }, 2);
+        }
+    }
+}
